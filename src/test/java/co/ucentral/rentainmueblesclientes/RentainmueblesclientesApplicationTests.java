ac765e125690414343b4be3e3f06078863074418
SebastianBueno12 <jbuenom1@ucentral.edu.co>
2024-04-28 13:39:48 -0500

    se agregaron todos los archivos del proyecto

---
+++ src/test/java/co/ucentral/rentainmueblesclientes/RentainmueblesclientesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.ucentral.rentainmueblesclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentainmueblesclientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
