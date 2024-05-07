4d2062370b11a3b4bf03cb14cf9281f3d63607a0
SebastianBueno12 <159496403+SebastianBueno12@users.noreply.github.com>
2024-05-06 19:08:25 -0500

    Revert "reistro de clientes inluyendo alunas dependecias en el pomp"

--- src/main/java/co/ucentral/rentainmueblesclientes/repositorios/ClienteRepositorio.java
+++
@@ -1,9 +0,0 @@
-package co.ucentral.rentainmueblesclientes.repositorios;
-
-import co.ucentral.rentainmueblesclientes.entidad.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClienteRepositorio extends JpaRepository<Cliente, Long> {
-}
