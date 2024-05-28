bf1a6c5e2b2666d53e5c1b6f1bbec91dd013a7fe
LealSantiago <159496797+LealSantiago@users.noreply.github.com>
2024-05-28 18:42:58 -0500

    Update BusquedaInmuebleServicio.java

--- src/main/java/co/ucentral/rentainmueblesclientes/servicio/BusquedaInmuebleServicio.java
+++ src/main/java/co/ucentral/rentainmueblesclientes/servicio/BusquedaInmuebleServicio.java
@@ -1,12 +1,12 @@
 package co.ucentral.rentainmueblesclientes.servicio;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import co.ucentral.rentainmueblesclientes.modelo.BusquedaInmueble;
 import co.ucentral.rentainmueblesclientes.repositorio.BusquedaInmuebleRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class BusquedaInmuebleServicio {
@@ -14,20 +14,16 @@ public class BusquedaInmuebleServicio {
     @Autowired
     private BusquedaInmuebleRepositorio repositorio;
 
-    /**
-     * Busca inmuebles basándose en varios criterios como la ciudad, fechas y número de personas.
-     * Útil para el Caso de Uso 01 cuando el usuario realiza una búsqueda básica o avanzada.
-     */
-    public List<BusquedaInmueble> buscarInmuebles(String ciudad, LocalDate fechaLlegada, LocalDate fechaSalida, Integer numPersonas) {
-        return repositorio.findByCriterios(ciudad, null, fechaLlegada, fechaSalida, numPersonas);
+    public List<BusquedaInmueble> buscarInmuebles(String ciudad, LocalDate fechaLlegada, LocalDate fechaSalida, Integer numPersonas,
+                                                  String zona, Boolean tienePiscina, Double precioMinimo, Double precioMaximo) {
+        try {
+            return repositorio.findByCriterios(ciudad, zona, fechaLlegada, fechaSalida, numPersonas, tienePiscina, precioMinimo, precioMaximo);
+        } catch (Exception e) {
+            throw new RuntimeException("Error al convertir las fechas", e);
+        }
     }
 
-    /**
-     * Obtiene los detalles de un inmueble específico, utilizada para visualizar información detallada en el CU15,
-     * y para proporcionar datos al formulario de reservas en el CU02.
-     */
     public BusquedaInmueble obtenerDetalleInmueble(Long id) {
-        Optional<BusquedaInmueble> inmueble = repositorio.findById(id);
-        return inmueble.orElse(null);  // Retorna el inmueble o null si no se encuentra, maneja el caso de consulta a detalle
+        return repositorio.findById(id).orElse(null);
     }
 }
